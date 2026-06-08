42ce7431684d3a53744553eac47cb7fba187e6c4
Aman Singh <amankrsingh764@gmail.com>
2026-06-08 20:35:51 +0530

    Add helper functions — load_pdf, split_text, get_embeddings

---
+++ src/helper.py
@@ -0,0 +1,35 @@
+from langchain_community.document_loaders import PyPDFLoader
+
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+from langchain_huggingface import HuggingFaceEmbeddings
+
+
+def load_pdf(path):
+
+    loader = PyPDFLoader(path)
+
+    documents = loader.load()
+
+    return documents
+
+
+def split_text(documents):
+
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200
+    )
+
+    chunks = splitter.split_documents(documents)
+
+    return chunks
+
+
+def get_embeddings():
+
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
+
+    return embeddings
